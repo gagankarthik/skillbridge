0c8d38d5184280ab68e03bb3835920193f1125f1
gagankarthik <gagankarthik123@gmail.com>
2025-06-25 19:29:44 -0400

    fast api

--- next.config.ts
+++ next.config.ts
@@ -12,4 +12,15 @@ module.exports = {
   },
 };
 
+module.exports = {
+  async rewrites(){
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/api/:path*', // Proxy to Backend
+      },
+    ];
+  }
+}
+
 export default nextConfig;
